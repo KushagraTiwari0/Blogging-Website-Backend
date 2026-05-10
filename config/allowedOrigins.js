60ebf1e67b2cea1b6ff7a6fa01bb715d59d0a6da
Kushagra Tiwari <kushagratiwari86@gmail.com>
2026-05-10 23:48:09 +0530

    Updated cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,9 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://blog-0qww.onrender.com'
+    'https://blog-0qww.onrender.com',
+    'https://undsund.in',
+    'https://www.undsund.in'
 
 ];
 
